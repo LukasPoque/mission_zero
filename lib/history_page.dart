c142ed560dc0a139e7f26705241f99496023e0bf
Lukas Poque <lukas.poque@rwth-aachen.de>
2020-02-18 16:06:57 +0100

    chart shows up, but buggy

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -24,21 +24,37 @@ class ConsumptionChart extends StatefulWidget {
   }
 }
 
-class _ConsumptionChartState extends State<ConsumptionChart>{
+class _ConsumptionChartState extends State<ConsumptionChart> {
   List<List<dynamic>> data = [];
-  List<charts.Series> seriesList;
+  List<charts.Series<ConsumerData, double>> seriesList = new List<charts.Series<ConsumerData, double>>();
+  bool ready = false;
 
   loadAssetData() async {
     final myData = await rootBundle.loadString("assets/power_sample.csv");
-    List<List<dynamic>> csvTable = CsvToListConverter(
-      eol: '\n'
-    ).convert(myData);
+    List<List<dynamic>> csvTable =
+        CsvToListConverter(eol: '\n').convert(myData);
     data = csvTable;
     createSeries();
   }
 
   createSeries() {
-
+    List<ConsumerData> dishwasherList = new List<ConsumerData>();
+    for (int i = 1; i < 100; i++) {
+      double time = double.parse(
+          (data[i][0].substring(13, 16).toString().replaceAll(":", ".")));
+      double consumption = data[i][1];
+      dishwasherList.add(new ConsumerData(time, consumption));
+    }
+    //geschirr
+    seriesList.add(new charts.Series<ConsumerData, double>(
+      id: "Dishwasher",
+      colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+      areaColorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault.lighter,
+      domainFn: (ConsumerData dataC, _) => dataC.timestamp,
+      measureFn: (ConsumerData dataC, _) => dataC.consumption,
+      data: dishwasherList,
+    ));
+    ready = true;
   }
 
   @override
@@ -49,12 +65,14 @@ class _ConsumptionChartState extends State<ConsumptionChart>{
 
   @override
   Widget build(BuildContext context) {
-    return seriesList == null ? CircularProgressIndicator() : StackedAreaCustomColorLineChart(seriesList);
+    return !ready
+        ? CircularProgressIndicator()
+        : StackedAreaCustomColorLineChart(seriesList);
   }
 }
 
 class StackedAreaCustomColorLineChart extends StatelessWidget {
-  final List<charts.Series> seriesList;
+  final List<charts.Series<ConsumerData, double>> seriesList;
 
   StackedAreaCustomColorLineChart(this.seriesList);
 
@@ -62,14 +80,14 @@ class StackedAreaCustomColorLineChart extends StatelessWidget {
   Widget build(BuildContext context) {
     return new charts.LineChart(seriesList,
         defaultRenderer:
-        new charts.LineRendererConfig(includeArea: true, stacked: true),
+            new charts.LineRendererConfig(includeArea: true, stacked: true),
         animate: false);
   }
 }
 
 class ConsumerData {
+  final double timestamp; //[3.10]
   final double consumption;
-  final DateTime timestamp;
 
-  ConsumerData(this.consumption, this.timestamp);
+  ConsumerData(this.timestamp, this.consumption);
 }
