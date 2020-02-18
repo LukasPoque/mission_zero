6fbbf2c8b39eac1397290b09a5243c376421583b
Lukas Poque <lukas.poque@rwth-aachen.de>
2020-02-18 15:12:16 +0100

    load csv file

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:csv/csv.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class HistoryPage extends StatelessWidget {
   final Color color;
@@ -7,8 +13,63 @@ class HistoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color,
-    );
+    return ConsumptionChart();
   }
+}
+
+class ConsumptionChart extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ConsumptionChartState();
+  }
+}
+
+class _ConsumptionChartState extends State<ConsumptionChart>{
+  List<List<dynamic>> data = [];
+  List<charts.Series> seriesList;
+
+  loadAssetData() async {
+    final myData = await rootBundle.loadString("assets/power_sample.csv");
+    List<List<dynamic>> csvTable = CsvToListConverter(
+      eol: '\n'
+    ).convert(myData);
+    data = csvTable;
+    createSeries();
+  }
+
+  createSeries() {
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadAssetData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return seriesList == null ? CircularProgressIndicator() : StackedAreaCustomColorLineChart(seriesList);
+  }
+}
+
+class StackedAreaCustomColorLineChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+
+  StackedAreaCustomColorLineChart(this.seriesList);
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.LineChart(seriesList,
+        defaultRenderer:
+        new charts.LineRendererConfig(includeArea: true, stacked: true),
+        animate: false);
+  }
+}
+
+class ConsumerData {
+  final double consumption;
+  final DateTime timestamp;
+
+  ConsumerData(this.consumption, this.timestamp);
 }
